55f293c670c8a3ae849c7a967269c31719bd79d8
Артём Лутфуллин <lutfullin.artem-a@net.ugatu.su>
2021-11-04 19:24:50 +0500

    синий фон и длинная форма

--- MyNotepad/MyNotepad/Form1.Designer.cs
+++ MyNotepad/MyNotepad/Form1.Designer.cs
@@ -68,8 +68,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.ClientSize = new System.Drawing.Size(400, 373);
+            this.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ClientSize = new System.Drawing.Size(902, 373);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button1);
