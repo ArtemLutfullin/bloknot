9850fdc366583fa611a7b92a7b8bc0539d1cb89d
Артём Лутфуллин <lutfullin.artem-a@net.ugatu.su>
2021-11-04 19:17:05 +0500

    start

--- MyNotepad/MyNotepad/Form1.Designer.cs
+++ MyNotepad/MyNotepad/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(106, 260);
+            this.button1.Location = new System.Drawing.Point(125, 248);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(122, 47);
             this.button1.TabIndex = 0;
